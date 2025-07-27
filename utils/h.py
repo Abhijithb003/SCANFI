515aa0bf6d8247d64b737e05d31313b08ad11bf1
Abhijith B <abhijithkulanjiyil@gmail.com>
2025-07-27 18:11:04 +0530

    your message

---
+++ utils/h.py
@@ -0,0 +1,9 @@
+a =int(input("enter a number"))
+b=int(input ("enter second number"))
+c=int(input("enter third no"))
+if a>b and a>c:
+      print("a is larger")
+elif b>a and b>c:
+    print("b is large")
+else:
+     print("c is large")
